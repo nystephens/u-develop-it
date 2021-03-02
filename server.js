a801800c9776cac5ec37827482fc6992bb1c9c23
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-03-02 09:55:45 -0800

    add post route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
+const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -62,18 +63,29 @@ app.delete('/api/candidate/:id', (req, res) => {
 
 
 // create candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//                 VALUES (?,?,?,?)`;
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors })
+        return;
+    }
 
-// const params = [1, 'Ronald', 'Firbank', 1];
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+                 VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
 
-// ES5 function, not arrow function to use this
-// db.run(sql,params, function(err, result) {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result, this.lastID);
-// });
+        res.json({
+            message: 'success',
+            data: body,
+            id: this.lastID
+        });
+    });
+});
 
 app.get('/api/candidates', (req, res) => {
     const sql = `SELECT * FROM candidates`;
